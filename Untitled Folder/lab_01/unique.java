d8e072ec2b9590d7d05ffa444e797c6d5e92fc5c
Jitendra Singh Rathore <jsroyal55@gmail.com>
2017-01-23 13:37:59 +0530

    folder

---
+++ Untitled Folder/lab_01/unique.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+public class unique
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner fileScanner = new Scanner(new File("hindi-document-00001.txt"));
+       //fileScanner.useDelimiter("[^A-Za-z0-9]");
+       ArrayList<String> words = new ArrayList<String>();
+      while (fileScanner.hasNext())
+       {
+        String nextWord = fileScanner.next();
+        if (!words.contains(nextWord))
+        {
+            words.add(nextWord);
+        }
+    }
+      Collections.sort(words);
+      System.out.println("There are " +  words.size()+" unique word(s)");
+      System.out.println("These words are:");
+      for (Iterator<String> it = words.iterator(); it.hasNext();) 
+      { String f = it.next();
+        System.out.println(f);
+      }
+      fileScanner.close();
+   }
+}
